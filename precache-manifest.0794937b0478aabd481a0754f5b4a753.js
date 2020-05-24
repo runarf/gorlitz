7e914e77a783f009fd21b823fb819e84cd3183f6
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2020-05-24 20:45:34 +0000

    Deploy runarf/gorlitz to github.com/runarf/gorlitz.git:gh-pages

---
+++ precache-manifest.0794937b0478aabd481a0754f5b4a753.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2027da62596bab36d111d7bb8e58adc5",
+    "url": "/gorlitz/index.html"
+  },
+  {
+    "revision": "33a20d1e565ca7e316da",
+    "url": "/gorlitz/static/css/2.48adc591.chunk.css"
+  },
+  {
+    "revision": "33a20d1e565ca7e316da",
+    "url": "/gorlitz/static/js/2.4436b44f.chunk.js"
+  },
+  {
+    "revision": "a52153bcc847b73a84a3dde72b2b9c2e",
+    "url": "/gorlitz/static/js/2.4436b44f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9ff51a6d5acc07400135",
+    "url": "/gorlitz/static/js/main.b94170bf.chunk.js"
+  },
+  {
+    "revision": "82d915fbca48bf7e3dca",
+    "url": "/gorlitz/static/js/runtime-main.57ad260e.js"
+  }
+]);
